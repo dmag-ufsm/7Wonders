540bcb61f6728f4be74f72831d44581e75be432a
Rafael Vales <rafaelvalesb@gmail.com>
2019-10-20 21:39:43 -0300

    add some required variables

--- src/card.cpp
+++ src/card.cpp
@@ -1,20 +1,38 @@
-#include "card.h"
+#include <card.h>
 #include <string>
 #include <utility>
 
 namespace DMAG {
 
-Card::Card(int id, std::string name) {
+Card::Card(){}
+
+Card::Card(int id, std::string name, int type, int era, std::vector<int> amount){
     this->id = id;
     this->name = std::move(name);
+    this->type = type;
+    this->era = era;
+    for (int i = 0; i < 5; i++)
+    	this->amount[i] = amount[i];
+}
+
+int Card::GetId() const{
+    return this->id;
 }
 
 std::string Card::GetName() const{
     return this->name;
 }
 
-int Card::GetId() const{
-    return this->id;
+int Card::GetType() const{
+    return this->type;
+}
+
+int Card::GetEra() const{
+	return this->era;
+}
+
+int Card::GetAmount(int players) const{
+	return this->amount[players-3];
 }
 
 }
