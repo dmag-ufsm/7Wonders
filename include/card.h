4366faf147f7b50cb4b9ea0aa1dd9ca6301260d2
Rafael Vales <rafaelvalesb@gmail.com>
2019-10-20 21:31:37 -0300

    add missing enums and required variables

--- include/card.h
+++ include/card.h
@@ -2,102 +2,119 @@
 #define CARD_H
 
 #include <string>
+#include <vector>
 
 namespace CARD_ID{
-	namespace MATERIALS{
-		enum{
-			lumber_yard,
-			stone_pit,
-			clay_pool,
-			ore_vein,
-			tree_farm,
-			excavation,
-			clay_pit,
-			timber_yard,
-			forest_cave,
-			sawmill,
-			quarry,
-			brickyard,
-			foundry,
+    namespace MATERIALS{
+        enum{
+            lumber_yard,
+            stone_pit,
+            clay_pool,
+            ore_vein,
+            tree_farm,
+            excavation,
+            clay_pit,
+            timber_yard,
+            forest_cave,
+            mine,
+            sawmill,
+            quarry,
+            brickyard,
+            foundry,
 
-		};
-	}
+        };
+    }
 
-	namespace MANUFACTURED{
-		enum{
-			loom,
-			glassworks,
-			press,
-		};
-	}
+    namespace MANUFACTURED{
+        enum{
+            loom,
+            glassworks,
+            press,
+        };
+    }
 
-	namespace CIVILIAN{
-		enum{
-			pawnshop,
-			baths,
-			altar,
-			theater,
-			aquaduct,
-			temple,
-			statue,
-			courthouse,
-			pantheon,
-			gardens,
-			town_hall,
-			palace,
-			senate,
-		};
-	}
+    namespace CIVILIAN{
+        enum{
+            altar,
+            theater,
+            pawnshop,
+            baths,
+            temple,
+            courthouse,
+            statue,
+            aqueduct,
+            gardens,
+            town_hall,
+            senate,
+            pantheon,
+            palace,
+        };
+    }
 
-	namespace COMMERCIAL{
-		enum{
-			tavern,
-			east_trading_post,
-			west_trading_post,
-			marketplace,
-			forum,
-			caravansery,
-			haven,
-			lighthouse,
-			chamber_of_commerce,
-			arena,
-		};
-	}
+    namespace COMMERCIAL{
+        enum{
+            tavern,
+            east_trading_post,
+            west_trading_post,
+            marketplace,
+            forum,
+            caravansery,
+            vineyard,
+            bazar,
+            haven,
+            lighthouse,
+            chamber_of_commerce,
+            arena,
+        };
+    }
 
-	namespace MILITARY{
-		enum{
-			stockade,
-			barracks,
-			guard_tower,
-			stables,
-			archery_range,
-		};
-	}
+    namespace MILITARY{
+        enum{
+            stockade,
+            barracks,
+            guard_tower,
+            walls,
+            training_ground,
+            stables,
+            archery_range,
+            fortifications,
+            circus,
+            arsenal,
+            siege_workshop,
+        };
+    }
 
-	namespace SCIENTIFIC{
-		enum{
-			apothecary,
-			workshop,
-			scriptorium,
-			dispensary,
-			laboratory,
-			library,
-		};
-	}
+    namespace SCIENTIFIC{
+        enum{
+            apothecary,
+            workshop,
+            scriptorium,
+            dispensary,
+            laboratory,
+            library,
+            school,
+            lodge,
+            observatory,
+            university,
+            academy,
+            study,
+        };
+    }
 
-	namespace GUILD{
-		enum{
-			workers,
-			craftsmen,
-			traders,
-			philosophers,spies,
-			strategists,
-			ship_owners,
-			scientists,
-			magistrates,
-			builders,
-		};
-	}
+    namespace GUILD{
+        enum{
+            workers,
+            craftsmens,
+            traders,
+            philosophers,
+            spies,
+            magistrates,
+            shipowners,
+            strategists,
+            scientists,
+            builders,
+        };
+    }
 
 }
 
@@ -121,11 +138,18 @@ class Card
 private:
     int id;
     std::string name;
+    int type;
+    int era;
+    int amount[5];
 
 public:
-    Card(int id, std::string name);
+    Card();
+    Card(int id, std::string name, int type, int era, std::vector<int> amount);
     [[nodiscard]] std::string GetName() const;
     [[nodiscard]] int GetId() const;
+    int GetType() const;
+    int GetEra() const;
+    int GetAmount(int) const;
 };
 
 }
