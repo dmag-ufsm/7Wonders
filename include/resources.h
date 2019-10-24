a845acb4b5e738be17b4bae74b64159f89b25d82
pprobst <pprobst@insiberia.net>
2019-10-23 21:20:48 -0300

    simplifying resource enum so it works better in a single map

--- include/resources.h
+++ include/resources.h
@@ -6,35 +6,56 @@
 #define INC_7WONDERS_RESOURCES_H
 
 namespace RESOURCE {
-    namespace MATERIALS {
-        enum {
-            lumber,
-            ore,
-            clay,
-            stone
-        };
-    }
-    namespace MANUFACTURE {
-        enum {
-            loom,
-            glassworks,
-            press
-        };
-    }
-    namespace SCIENCE {
-        enum {
-            engineering, // cogs
-            navigation,  // compass
-            writing      // tablet
-        };
-    }
-    namespace OTHER {
-        enum {
-            coin,
-            victory,
-            battle
-        };
-    }
+    enum {
+        wood,
+        ore,
+        clay,
+        stone,
+
+        cloth,
+        glass,
+        papyrus,
+
+        gear,
+        compass,
+        tablet
+    };
 }
+
+/* I decided to simplify things a little bit so it'll work with a single map with all the needed resources.
+* I hope you're okay with that.
+
+namespace RESOURCE {
+namespace MATERIALS {
+    enum {
+        wood,
+        ore,
+        clay,
+        stone
+    };
+}
+namespace MANUFACTURE {
+    enum {
+        cloth,
+        glass,
+        papyrus
+    };
+}
+namespace SCIENCE {
+    enum {
+        gear,
+        compass,
+        tablet
+    };
+}
+namespace OTHER {
+    enum {
+        coin,
+        victory,
+        battle
+    };
+}
+}
+*/
 
 #endif //INC_7WONDERS_RESOURCES_H
