e18a7f62d552a138b3ef33e82ce85fc824525aca
Julio Pinto Coelho <juliopcrj@gmail.com>
2020-03-08 19:02:35 -0300

    Adding json guide file

---
+++ JsonGuide.js
@@ -0,0 +1,13 @@
+// comentário
+
+{
+    "command":
+    {
+        "subcommand": "alguma ação", // "build_structure", "build_guild"...
+        "argument": "nome de alguma carta ou recurso", //"não lembro os nomes das cartas hue"
+    }
+}
+
+ //Isso é um arquivo json.
+//Os nomes dos arquivos que vão ser parsed pelo progrma são
+//player_%d.json, onde %d é o índice do player.
