b5399bf87ed6b9a9c50a1e0d093b3f98ef40b32d
Rafael Vales <rafaelvalesb@gmail.com>
2020-03-25 19:47:30 -0300

    Update GUI.py

--- test/GUI.py
+++ test/GUI.py
@@ -1,11 +1,7 @@
 from tkinter import *
 import json
 
-w_width = 1250
-w_height = 750
-
 players = 3
-res = w_width / 1250
 
 root = Tk()
 root.wm_title('7 Wonders')
@@ -13,14 +9,35 @@ root.wm_title('7 Wonders')
 with open('game_status.json') as f:
     game_json = json.load(f)
 
+w_width = 1300
+w_height = 665
 canvas = Canvas(root, width=w_width, height=w_height, bg='white')
 canvas.pack(expand=YES, fill=BOTH)
 
-def img_resize(img, n):
-    prec = 10
-    img = img.zoom(int(n * prec))
-    img = img.subsample(prec)
+def load_image(local, f):
+    f_z, f_ss = f
+    img = PhotoImage(file=local)
+    if f_z != 1:
+        img = img.zoom(f_z)
+    if f_ss != 1:
+        img = img.subsample(f_ss)
     return img
+
+###############
+size = {
+    'wonder' : (2, 3),
+    'wonder_stage' : (2, 3),
+    'hand_cards' : (1, 4),
+    'played_cards' : (1, 5),
+    'resources' : (1, 4)
+}
+img = {
+    'wonder' : load_image('images/wonders/board_0.png', size['wonder']),
+    'wonder_stage' : load_image('images/wonders/wonder_stage.png', size['wonder_stage']),
+    'hand_cards' : load_image('images/cards/Mine.png', size['hand_cards']),
+    'played_cards' : load_image('images/cards/Mine.png', size['played_cards']),
+    'resources' : load_image('images/resources/wood.png', size['resources'])
+}
 
 ############### WONDERS ###############
 img_wonder = [None]*players
@@ -28,15 +45,13 @@ img_wonder_stage = [None]*players
 for i in range(players):
     wonder_id = game_json['players'][str(i)]['wonder_id']
     wonder_stage = game_json['players'][str(i)]['wonder_stage']
-    img_wonder[i] = PhotoImage(file='images/wonders/board_'+str(wonder_id)+'.png')
-    img_wonder[i] = img_resize(img_wonder[i], 0.6*res)
-    canvas.create_image(5*res + (w_width/players)*i, 5*res, image=img_wonder[i], anchor=NW)
+    img_wonder[i] = load_image('images/wonders/board_'+str(wonder_id)+'.png', size['wonder'])
+    canvas.create_image(5 + (w_width/players)*i, 5, image=img_wonder[i], anchor=NW)
     img_wonder_stage[i] = [None]*wonder_stage
     for j in range(wonder_stage):
-        img_wonder_stage[i][j] = PhotoImage(file='images/wonders/wonder_stage.png')
-        img_wonder_stage[i][j] = img_resize(img_wonder_stage[i][j], 0.6*res)
-        x = 5*res + (w_width/players)*i + img_wonder[0].width()*0.065 + j*(img_wonder_stage[0][0].width() + img_wonder[0].width()*0.035)
-        y = 5*res + img_wonder[0].height()
+        img_wonder_stage[i][j] = load_image('images/wonders/wonder_stage.png', size['wonder_stage'])
+        x = 5 + (w_width/players)*i + img['wonder'].width()*0.065 + j*(img['wonder_stage'].width() + img['wonder'].width()*0.035)
+        y = 5 + img['wonder'].height()
         canvas.create_image(x, y, image=img_wonder_stage[i][j], anchor=NW)
 ###############
 
@@ -46,10 +61,9 @@ for i in range(players):
     cards_hand = game_json['players'][str(i)]['cards_hand']
     img_hand_cards[i] = [None]*len(cards_hand)
     for j, c in enumerate(cards_hand):
-        img_hand_cards[i][j] = PhotoImage(file='images/cards/' + c + '.png')
-        img_hand_cards[i][j] = img_resize(img_hand_cards[i][j], 0.2*res)
-        x = 5*res + (w_width/players)*i
-        y = img_wonder[0].height() + img_wonder_stage[0][0].height() + 5*(j+2)*res + img_hand_cards[0][0].height()*j
+        img_hand_cards[i][j] = load_image('images/cards/' + c + '.png', size['hand_cards'])
+        x = 5 + (w_width/players)*i
+        y = img['wonder'].height() + img['wonder_stage'].height() + 5*(j+2) + img['hand_cards'].height()*j
         canvas.create_image(x, y, image=img_hand_cards[i][j], anchor=NW)
 ###############
 
@@ -67,21 +81,30 @@ for i in range(players):
         if p[r] == -1:
             continue
         n = resources[r] if resources[r] < 10 else 10
-        img_resources[i][j] = PhotoImage(file='images/resources/' + r + '.png')
-        img_resources[i][j] = img_resize(img_resources[i][j], 0.30*res)
-        x = 10*res + (w_width/players)*i + img_hand_cards[0][0].width() + (5*res+img_resources[0][0].width())*p[r]
-        y = 10*res + img_wonder[0].height() + img_wonder_stage[0][0].height()
+        img_resources[i][j] = load_image('images/resources/' + r + '.png', size['resources'])
+        x = 10 + (w_width/players)*i + img['hand_cards'].width() + (5+img['resources'].width())*p[r]
+        y = 10 + img['wonder'].height() + img['wonder_stage'].height()
         canvas.create_image(x, y, image=img_resources[i][j], anchor=NW)
-        img_value[i][j] = PhotoImage(file='images/resources/' + str(n) + '.png')
-        img_value[i][j] = img_resize(img_value[i][j], 0.30*res)
-        x = 10*res + (w_width/players)*i + img_hand_cards[0][0].width() + (5*res+img_resources[0][0].width())*p[r]
-        y = 10*res + img_wonder[0].height() + img_wonder_stage[0][0].height() + img_resources[0][0].height()
+        img_value[i][j] = load_image('images/resources/' + str(n) + '.png', size['resources'])
+        x = 10 + (w_width/players)*i + img['hand_cards'].width() + (5+img['resources'].width())*p[r]
+        y = 10 + img['wonder'].height() + img['wonder_stage'].height() + img['resources'].height()
         canvas.create_image(x, y, image=img_value[i][j], anchor=NW)
         j = j + 1
 ###############
 
 ############# PLAYED CARDS ###############
-
+img_played_cards = [None]*players
+for i in range(players):
+    cards_played = game_json['players'][str(i)]['cards_played']
+    img_played_cards[i] = [None]*len(cards_played)
+    for j, c in enumerate(cards_played):
+        img_played_cards[i][j] = load_image('images/cards/' + c + '.png', size['played_cards'])
+        x = 10 + (w_width/players)*i + img['hand_cards'].width()
+        y = 18 + img['wonder'].height() + img['wonder_stage'].height() + 2*img['resources'].height() + (j%9)*img['played_cards'].height()*0.9
+        if j >= 9:
+            x = x + 5 + img['played_cards'].width()
+        canvas.create_image(x, y, image=img_played_cards[i][j], anchor=NW)
+        j = j + 1
 ###############
 
 root.mainloop()
